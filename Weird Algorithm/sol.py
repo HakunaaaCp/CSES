29f25d25e746eb130eb01ce85564175b81955af2
Hakunaaa <dalaitserenganbold@Dalaitserens-MacBook-Air.local>
2025-09-16 16:32:18 +0200

    Add solutions to Weird Algorithm

---
+++ Weird Algorithm/sol.py
@@ -0,0 +1,12 @@
+def main():
+    n = int(input())
+    while n != 1:
+        print(n, end=" ")
+        if n % 2 == 0:
+            n //= 2  
+        else:
+            n = n * 3 + 1
+    print(1)
+
+if __name__ == "__main__":
+    main()
